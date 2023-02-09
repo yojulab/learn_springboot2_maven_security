cf5e2cc78254108767088985e7a23fc6f7ba22b2
yojulab <otter.oh@gmail.com>
2023-02-09 15:21:23 +0900

    add some files

--- src/main/java/com/study/study_springboot_security/controller/MemberWithAuthorityController.java
+++ src/main/java/com/study/study_springboot_security/controller/MemberWithAuthorityController.java
@@ -1,13 +1,15 @@
 package com.study.study_springboot_security.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class MemberWithAuthorityController {
     @RequestMapping(value = "/joinForm", method = RequestMethod.GET)
     public ModelAndView joinForm(ModelAndView modelAndView){
-        String viewName = "/WEB-INF/views/joinForm.jsp";
+        String viewName = "/WEB-INF/views/member/joinForm.jsp";
 
         modelAndView.setViewName(viewName);
         return modelAndView;
