800042b40a28a9e17c3511787fdc30a7ac9a0286
Sanghun Oh <otter.oh@gmail.com>
2023-03-03 23:28:36 +0900

    fix some path

--- src/main/java/com/learn/learn_springboot_security/aspects/LoggingAspect.java
+++ src/main/java/com/learn/learn_springboot_security/aspects/LoggingAspect.java
@@ -7,16 +7,19 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-@Aspect	
+@Aspect
 @Component
 public class LoggingAspect {
-private Logger logger = Logger.getLogger(getClass().getName());
-@Before(value="execution(* com.clustering.project.controller.*Controller.*(..))")
-public void logBefore(JoinPoint joinPoint){
-logger.info("Entering " + joinPoint.getTarget().getClass().getSimpleName() 
-+"'s"+ joinPoint.getSignature().getName());
-Object[] args = joinPoint.getArgs();
-for(int i=0; i<args.length; i++){
-logger.info("LoggingAspect args["+ i +"] --> "+ args[i]);		}
-}
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    // src/main/java/com/learn/learn_springboot_security/controller
+    @Before(value = "execution(* com.learn.learn_springboot_security.controller.*Controller.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        logger.info("Entering " + joinPoint.getTarget().getClass().getSimpleName()
+                + "'s" + joinPoint.getSignature().getName());
+        Object[] args = joinPoint.getArgs();
+        for (int i = 0; i < args.length; i++) {
+            logger.info("LoggingAspect args[" + i + "] --> " + args[i]);
+        }
+    }
 }
